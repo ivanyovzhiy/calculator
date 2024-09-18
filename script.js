b1276ec123eef9190919d32071f4d2b70374ce7f
Ivan yovzhiy <ivanyovzhiy@Ivans-MBP.lan>
2024-09-17 22:19:58 -0700

    updated

---
+++ script.js
@@ -0,0 +1,10 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalButtons = document.querySelector('[data-equals]')
+const deleteButtons = document.querySelector('[data-delete]')
+const allClearButtons = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+
+
